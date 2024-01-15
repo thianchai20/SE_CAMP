b237401789bdb5362a43580d2332d1009827b307
thianchai20 <152171290+thianchai20@users.noreply.github.com>
2024-01-15 19:41:44 +0700

    sub

--- index.php
+++ index.php
@@ -4,27 +4,41 @@
     <meta charset="utf-8">
 </head>
 
-<body>
-    <?php $multi_x = 2; ?>
-    <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
+<body style="background-color : pink">
+    <center>
+        <?php $multi_x = 2; ?>
+        <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
+        <div>
+            <table>
+                <tbody>
+                    <?php
+                    for ($i = 1; $i <= 24; $i++) {
+                    ?>
+                        <tr>
+                            <td><?php echo $i ?></td>
+                            <td>x</td>
+                            <td><?php echo $multi_x ?></td>
+                            <td>=</td>
+                            <td><?php echo $i * $multi_x ?></td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+    </center>
 
-    <table>
-        <tbody>
-            <tr>
-                <td></td>
-                <td>x</td>
-                <td></td>
-                <td>=</td>
-                <td></td>
-            </tr>
-        </tbody>
-    </table>
     <style>
-        table {
-            width: 150px;
-            height: 265px;
+        div {
+            background-color: white;
+            width: 220px;
+            height: 380px;
+            border: 1px solid black;
+            overflow-y: scroll;
+
         }
     </style>
+
+
 </body>
 
 </html>
